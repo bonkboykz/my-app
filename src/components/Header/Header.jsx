939602cdac1d7475cce81762d13cd03e30994304
Yesset <poxerface5@gmail.com>
2024-09-30 21:18:27 +0500

    feat: logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
 import "./Header.css";
+import { useDispatch } from "react-redux";
+
+import { logout } from "../../store/actions/auth";
+import { useNavigate } from "react-router-dom";
 
 export function Header({ className }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const vyitiOtsuda = () => {
+    dispatch(logout());
+    navigate("/auth/login");
+  };
+
   return (
     <div className={`header ${className}`}>
       <div className="header__left-part">
@@ -12,9 +24,14 @@ export function Header({ className }) {
         </div>
       </div>
 
-      {/* User profile round avatar */}
-      <div className="header__profile">
-        <img src="https://via.placeholder.com/150" alt="profile" />
+      <div>
+        {/* User profile round avatar */}
+        <div className="header__profile">
+          <img src="https://via.placeholder.com/150" alt="profile" />
+        </div>
+        <button type="submit" onClick={vyitiOtsuda}>
+          Logout
+        </button>
       </div>
     </div>
   );
